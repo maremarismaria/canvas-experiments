89917a8f117ffb3218b1b5953841dc24eb8673c4
maremarismaria <mariamoralespadron@gmail.com>
2018-01-06 18:06:53 +0000

    fibonacci added

--- script.js
+++ script.js
@@ -11,6 +11,32 @@ function randomColor(){
 
 function getValue(elementId){
     var value = document.getElementById(elementId).value;
-    document.getElementById("output").innerHTML = value;
+    var number = parseInt(value);
+    
+    document.getElementById("output").innerHTML = "F(" + value + ")";
+    console.log(number + "->" + fibonacciSequence(number));
     document.getElementById("output").style.color = randomColor();
 }
+
+function fibonacciSequence(number){
+
+    var sequence = [];
+    var j = 0;
+
+    for (var i = 0; i <= number; i++) {
+        
+        if(i < 3){
+            sequence.push(j);
+            j++;
+        }
+
+        if(i > 2 && i < number){
+            var penultimate = sequence[sequence.length - 2];
+            var last = sequence[sequence.length - 1];
+            sequence.push(penultimate + last);
+        }
+    }
+    
+    return sequence;
+}
+
